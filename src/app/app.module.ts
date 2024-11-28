2d2a32e97c0fca5547f22927f994e90489b2e859
Manjeet Singh <98460132+manjeetdeveloper@users.noreply.github.com>
2024-11-28 12:31:45 +0530

    new upadted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule,ReactiveFormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,61 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
 })
 export class Tab1Page {
+  registrationForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder, private alertCtrl: AlertController) {
+    this.registrationForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      age: ['', [Validators.required, Validators.min(18)]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+    });
+  }
 
+  get name() {
+    return this.registrationForm.get('name');
+  }
+
+  get email() {
+    return this.registrationForm.get('email');
+  }
+
+  get password() {
+    return this.registrationForm.get('password');
+  }
+
+  get age() {
+    return this.registrationForm.get('age');
+  }
+
+  get phone() {
+    return this.registrationForm.get('phone');
+  }
+
+  async onRegister() {
+    if (this.registrationForm.valid) {
+      const formData = this.registrationForm.value;
+
+      // Create an alert to display the success message
+      const alert = await this.alertCtrl.create({
+        header: 'Registered Successfully',
+        message: 'Your registration has been completed successfully!',
+        buttons: ['OK'],
+      });
+
+      // Show the alert
+      await alert.present();
+
+      // Reset the form after submission
+      this.registrationForm.reset();
+    }
+  }
 }
